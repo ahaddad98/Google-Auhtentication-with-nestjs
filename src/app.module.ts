39f7c2cfee16184ecf19a0c0d86c3b0d3fde9f51
ahaddad98 <amine.haddadd@gmail.com>
2022-02-16 18:53:45 +0100

    Auth google

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GoogleStrategy } from './googleauth/google.strategy'
+import { GoogleauthService } from './googleauth/googleauth.service';
 
 @Module({
-  imports: [],
+  imports: [GoogleauthService],
   controllers: [AppController],
-  providers: [AppService, GoogleStrategy],
+  providers: [AppService, GoogleStrategy, GoogleauthService],
+  // exports: [GoogleauthService],
 })
 export class AppModule {}
--- src/googleauth/googleauth.module.ts
+++ src/googleauth/googleauth.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { GoogleauthService } from './googleauth.service';
 
 @Module({
-  providers: [GoogleauthService]
+  providers: [GoogleauthService],
+  exports: [GoogleauthService]
+
 })
 export class GoogleauthModule {}
